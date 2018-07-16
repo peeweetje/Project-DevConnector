3c23ad86d06fcb9605041e3016949e812cce0d58
KODISION\pvos <p.vos@kodision.nl>
2018-07-16 16:34:35 +0200

    refactor: add mongoDB to connect with database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to mogoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB is connnected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello!"));
 
