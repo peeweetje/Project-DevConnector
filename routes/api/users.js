224e4f3e91fcef354c57728f8abda4fc6cc622ec
Petra Vos <petravos70@gmail.com>
2018-07-23 12:44:43 +0200

    feat: add new folder validation and files. add register validation to user

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User Model
 const User = require("../../models/Users");
 
@@ -18,9 +21,16 @@ router.get("/test", (req, res) => res.json({ msg: "users route" }));
 // @desc Register user
 // @acess Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // size
