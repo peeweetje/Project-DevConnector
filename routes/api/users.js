e794a8ca3a25a56dbe0abf0da392e1952726e1d3
Petra Vos <petravos70@gmail.com>
2018-07-25 20:13:49 +0200

    refactor: change error messages

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
-
+const validateLoginInput = require("../../validation/login");
 // Load User Model
 const User = require("../../models/Users");
 
@@ -23,6 +23,7 @@ router.get("/test", (req, res) => res.json({ msg: "users route" }));
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
+  // Check validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -61,6 +62,13 @@ router.post("/register", (req, res) => {
 // @desc Login user / Returning JWT token
 // @acess Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -68,7 +76,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "user not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
@@ -89,7 +98,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
