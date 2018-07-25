0988f3d869eaaaac2ad4c3d616501a39cfb7dd3e
Petra Vos <petravos70@gmail.com>
2018-07-25 12:59:24 +0200

    refactor: add validation to name email and password

--- validation/register.js
+++ validation/register.js
@@ -3,8 +3,42 @@ const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is require";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "E-mail field is require";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "E-mail is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is require";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password field is require";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = " confirm Password field is require";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = " Passwords must match";
   }
 
   return {
