0cab01236ea60301093865cc7fa34b0706dff2dc
Petra Vos <petravos70@gmail.com>
2018-07-25 20:14:30 +0200

    refactor: add validation for login

---
+++ validation/login.js
@@ -0,0 +1,26 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "E-mail is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "E-mail field is require";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is require";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
