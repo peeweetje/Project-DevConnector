f0aa29016b7b29d639db4adccc301514154dbe0a
Petra Vos <petravos70@gmail.com>
2018-07-27 14:17:21 +0200

    add mongoose and passport. change the error handling

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile model
+const Profile = require("../../models/Profile");
+
+// Load user model
+const User = require("../../models/Users");
 
 // @route GET api/profile/test
 // @desc  Tests profile route
 // @acess Public
 router.get("/test", (req, res) => res.json({ msg: "profile route" }));
+
+// @route GET api/profile
+// @desc Get current users profile profile
+// @acess Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
