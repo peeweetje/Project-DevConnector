15d449b0574142367924f5ac3ce181b52cfe2fef
Petra Vos <petravos70@gmail.com>
2018-08-13 13:21:48 +0200

    add route to creat-profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+
 import store from "./store";
 import jwt_token from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
